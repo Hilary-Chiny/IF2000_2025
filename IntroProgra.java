43d5f3a8a1d014d7efe90703d405538b8375d85f
HilaryChiny <chinycastillohilaryfabiola@gmail.com>
2025-08-14 11:28:13 -0600

    Mi primer commit en Java

---
+++ IntroProgra.java
@@ -0,0 +1,10 @@
+public class IntroProgra {
+    public static void main(String[] args) {
+       //Data types
+       String name = "Hilary";
+        int age = 18;
+        System.out.println ("Hello world");
+        System.out.println ("Name:" + name);
+        System.out.println ("Age:" + age);
+    }
+}
